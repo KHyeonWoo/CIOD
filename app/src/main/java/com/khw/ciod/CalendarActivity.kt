fc67b968baa941744368d25cd33e1edd38d5c141
bokdong <altmen77@naver.com>
2024-06-10 11:43:27 +0900

    Current Day Check

--- app/src/main/java/com/khw/ciod/CalendarActivity.kt
+++ app/src/main/java/com/khw/ciod/CalendarActivity.kt
@@ -9,18 +9,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
@@ -41,13 +42,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -78,7 +79,8 @@ class CalendarActivity : ComponentActivity() {
         val db by remember { mutableStateOf(AppDatabase.getDatabase(context)) }
 
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
                 .background(Color(0xFFDCE9EC))
         ) {
             CalendarHeader(year, month, onPreviousMonth = {
@@ -134,12 +136,28 @@ class CalendarActivity : ComponentActivity() {
             }
         }
 
+        val currentDay = LocalDate.now().dayOfMonth
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(40.dp), contentAlignment = Alignment.Center
         ) {
-            Text(text = "$day", fontSize = 16.sp)
+            if (day == currentDay) {
+                Text(
+                    text = "$day",
+                    fontSize = 16.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxHeight()
+                        .width(32.dp)
+                        .background(
+                            Color(0xFF50B4B0), RoundedCornerShape(10.dp)
+                        )
+                )
+
+            } else {
+                Text(text = "$day", fontSize = 16.sp)
+            }
         }
 
         Box(
